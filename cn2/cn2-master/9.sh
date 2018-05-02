4284f2f166d2dd9ae6cc928c4c2e3b934e3b936f
arun s acharya <38354756+arunsacharya@users.noreply.github.com>
2018-05-02 11:14:21 +0530

    Delete 9.sh

--- cn2/cn2-master/9.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Choose the typre of protocol in the TCL file:- AODV;DSDV;DSR" 
-echo "Running TCL file"
-ns 9.tcl
-echo "TCL file completed. Running NAM file."
-nam 9.nam &
-echo "END of program"
