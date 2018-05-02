d5941c5493c445da8fee3f72e17e54b2f199be5e
arun s acharya <38354756+arunsacharya@users.noreply.github.com>
2018-05-02 11:09:08 +0530

    Delete 10.sh

--- cn2/cn2-master/10.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#echo "Producing TCP file TCP_10"
-#ns /root/Downloads/ns-allinone-2.35/ns-2.35/indep-utils/cmu-scen-gen/cbrgen.tcl > /root/CN/10/TCP_10
-echo "Running TCL file"
-ns 10.tcl
-echo "Running NAM file"
-nam 10.nam &
-echo "END of program 10"
